d23211d244123bdc01baa496a94661a3a3dda8bf
Khalid Obaide <sayedkhalidobaide@gmail.com>
2019-10-22 18:51:54 +0800

    Added The Data Base

--- home/views.py
+++ home/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    if request.method != 'POST':
+        context = {}
+        return render(request, 'index.html', context)
+    else:
+        try:
+            username = request.POST['username']
+            password = request.POST['password']
+        except:
+            return HttpResponse('Some Problem cannot get the fields ')
+
+        return HttpResponse('YAYYAY Logged in ')
